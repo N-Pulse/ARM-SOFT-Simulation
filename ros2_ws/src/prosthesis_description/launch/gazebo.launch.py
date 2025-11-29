e3ec5c506fda88db46506c75544334d65f2ef9f2
Enaira20 <orianne.farinelli@epfl.ch>
2025-11-29 15:58:06 +0100

    basic ros2 control (#5)
    added Gazebo-ros plugins
    
    added basic controllers : Use of joint_state_broadcaster which can publish the state of the registered hardware interfaces and joint_trajectory_controller which can create an action when receiving a trajectory for the robot to execute.
    
    implemented script movement_input.py which can send a trajectory to the joint_trajectory_controller. When run we see the prosthesis move in gz simulation.
    
    implemented script joint_state_printer.py which subscribes to the topic /joint_states and prints infos for each joint.

---
+++ ros2_ws/src/prosthesis_description/launch/gazebo.launch.py
@@ -0,0 +1,88 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    pkg_name = 'prosthesis_description'
+    pkg_share = get_package_share_directory(pkg_name)
+    world_file = os.path.join(pkg_share, 'worlds', 'empty.sdf')
+    
+    # Include the base robot_state_publisher launch file
+    robot_state_publisher_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(pkg_share, 'launch', 'robot_state_publisher.launch.py')
+        ]),
+        launch_arguments={
+            'use_sim_time': 'true'  # Using simulation time in Gazebo
+        }.items()
+    )
+    
+    # Include the Gazebo launch file, provided by the gazebo_ros package
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(get_package_share_directory('ros_gz_sim'), 
+                        'launch', 'gz_sim.launch.py')
+        ]),
+        launch_arguments={'gz_args': f'-r {world_file}'}.items()
+    )
+    
+    # Run the spawner node from the gazebo_ros package after a delay to ensure Gazebo is ready
+    spawn_entity = TimerAction(
+        period=5.0,  # Wait 5 seconds for Gazebo to start
+        actions=[
+            Node(
+                package='ros_gz_sim',
+                executable='create',
+                arguments=[
+                    '-topic', 'robot_description',
+                    '-name', 'prosthesis',
+                    '-x', '0.0',
+                    '-y', '0.0', 
+                    '-z', '0.2'
+                ],
+                parameters=[{
+                    'use_sim_time': True
+                }],
+                output='screen'
+            )
+        ]
+    )
+
+    # Clock bridge node - converts Gazebo clock to ROS clock
+    clock_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=['clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'],
+        output='screen',
+        parameters=[{
+            'use_sim_time': True
+        }]
+    )
+
+    # Nodes to automatically configure joint_state_broadcaster and joint_state_controller
+    load_joint_state_broadcaster = Node(
+    package='controller_manager',
+    executable='spawner',
+    arguments=['joint_state_broadcaster'],
+    output='screen'
+    )
+
+    load_joint_trajectory_controller = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['joint_trajectory_controller'],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        robot_state_publisher_launch,
+        gazebo,
+        spawn_entity,
+        clock_bridge,
+        load_joint_state_broadcaster,
+        load_joint_trajectory_controller
+    ])
---
+++ ros2_ws/src/prosthesis_description/scripts/movement_input.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+# Node that sends a joint trajectory goal to a ROS2 action server (which is read by joint_trajectory_controller)
+
+import rclpy
+from rclpy.node import Node
+from rclpy.action import ActionClient
+
+from control_msgs.action import FollowJointTrajectory
+from trajectory_msgs.msg import JointTrajectoryPoint
+
+class TrajectoryClient(Node):
+
+    def __init__(self):
+        super().__init__('trajectory_client')
+
+        self._client = ActionClient(
+            self,
+            FollowJointTrajectory,
+            '/joint_trajectory_controller/follow_joint_trajectory'
+        )
+
+    def send_goal(self):
+        self._client.wait_for_server()
+
+        goal_msg = FollowJointTrajectory.Goal()
+
+        goal_msg.trajectory.joint_names = [
+            "joint_wrist_to_palm",
+            "joint_index_mcp",
+            "joint_index_pip",
+            "joint_middle_mcp",
+            "joint_middle_pip",
+            "joint_ring_mcp",
+            "joint_ring_pip",
+            "joint_little_mcp",
+            "joint_little_pip",
+            "joint_thumb_cmc",
+            "joint_thumb_mcp",
+            "joint_thumb_ip"
+        ]
+
+        point = JointTrajectoryPoint()
+        point.positions = [
+            0.2,
+            0.5,
+            0.5,
+            0.5,
+            0.5,
+            0.5,
+            0.5,
+            0.5,
+            0.5,
+            0.5,
+            0.5,
+            0.5
+        ]
+
+        point.time_from_start.sec = 2
+
+        goal_msg.trajectory.points.append(point)
+
+        self.get_logger().info("Sending trajectory...")
+        send_goal_future = self._client.send_goal_async(goal_msg)
+        rclpy.spin_until_future_complete(self, send_goal_future)
+
+
+def main():
+    rclpy.init()
+    node = TrajectoryClient()
+    node.send_goal()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
